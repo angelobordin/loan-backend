6e975504944aea5838d3ebe8cd2f64541b48189a
Angelo Bordin <angelombordin@gmail.com>
2024-08-27 00:49:48 -0300

    implements sql querys and create tables

---
+++ src/main/resources/db/migration/V2__create_loan_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE loan (
+    id SERIAL PRIMARY KEY,
+    dataEmprestimo TIMESTAMP NOT NULL,
+    moeda VARCHAR(3) NOT NULL,
+    valorObtido NUMERIC NOT NULL,
+    dataVencimento TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    customer_id INTEGER,
+    FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
+);
