0eeddb434ea3f85340679d1046813abe3df535b7
Angelo Bordin <angelombordin@gmail.com>
2024-08-30 05:58:21 -0300

    create auxiliar functions

--- src/main/java/com/loan_calculator/api/repositories/LoanRepository.java
+++ src/main/java/com/loan_calculator/api/repositories/LoanRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.loan_calculator.api.domain.loan.Loan;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
-
+    long countByCustomerId(Long customerId);
 }
